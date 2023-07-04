d0fdda1a1513079dad176b2aa9db70037506e835
lohi059 <lohithlenka123@gmail.com>
2023-07-04 23:39:42 +0530

    authentication

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,2 +1,22 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+router.put("/:id",async(req,res)=>{
+    if(req.body.userId === req.params.id){
+        if(req.body.password){
+            const salt = await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(req.body.password,salt);
+        }
+        try{
+            const updatedUser = await User.findById(req.params.id,{
+                $set:req.body,
+            })
+        }catch(err){
+            res.status(500).json(err);
+        }
+    }else{
+        res.status(401).json("you can update only your account")
+    }   
+})
+
